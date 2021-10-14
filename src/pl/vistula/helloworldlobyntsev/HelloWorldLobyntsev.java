6a2fd6b8eb596da6adbb97649b3c6419dc45258f
Arsenii Lobyntsev <arseniylobyntsev@gmail.com>
2021-10-14 17:11:08 +0300

    Initial commit

---
+++ src/pl/vistula/helloworldlobyntsev/HelloWorldLobyntsev.java
@@ -0,0 +1,8 @@
+package pl.vistula.helloworldlobyntsev;
+
+public class HelloWorldLobyntsev {
+    public static void main(String[] args) {
+        System.out.println("Hello Vistula Lobyntsev");
+        System.out.println("Goodbye Vistula Lobyntsev");
+    }
+}
