7eda8853996224c1a7015ec3b63534746f7ccdb6
Arsenii Lobyntsev <arseniylobyntsev@gmail.com>
2021-10-14 17:28:21 +0300

    Changed

--- src/pl/vistula/helloworldlobyntsev/HelloWorldLobyntsev.java
+++ src/pl/vistula/helloworldlobyntsev/HelloWorldLobyntsev.java
@@ -2,7 +2,6 @@ package pl.vistula.helloworldlobyntsev;
 
 public class HelloWorldLobyntsev {
     public static void main(String[] args) {
-        System.out.println("Hello Vistula Lobyntsev");
-        System.out.println("Goodbye Vistula Lobyntsev");
+        System.out.println("Hello Vistula from Arsenii Lobyntsev! (changed)");
     }
 }
