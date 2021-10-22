5808636e33f3aa82ee44112c551af494eaef8d1a
Arsenii Lobyntsev <arseniylobyntsev@gmail.com>
2021-10-22 09:35:38 +0300

    Zadania z ćwiczeń

--- src/pl/vistula/helloworldlobyntsev/HelloWorldLobyntsev.java
+++
@@ -1,9 +0,0 @@
-package pl.vistula.helloworldlobyntsev;
-
-public class HelloWorldLobyntsev {
-    public static void main(String[] args) {
-        System.out.println("Hello Vistula from Arsenii Lobyntsev!");
-        System.out.println("Hello for the second time!");
-        System.out.println("And farewell.");
-    }
-}
